ae5d2b64ff2c0a38417a79164151779408c49b40
Lazybird-Chao <liuchao0153@hotmail.com>
2017-03-30 22:56:20 -0400

    no important

---
+++ utc-0.1/benchapps/nbody/seq/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# mpi command
+#MPI_RUN='mpirun -report-bindings -bind-to none' #socket:overload-allowed'
+#MPI_PROCS=1
+#MPI_OPTION="-host node2,node3,node4,node5 -N ${MPI_PROCS}"
+#MPI_OPTION="-N ${MPI_PROCS}  -hostfile myhosts -host node2,node3,node4,node5"
+
+
+# set exe file and args
+EXE_FILE='./nbody_main'
+EXE_ARGS="-n 32768"
+
+# set iterations and run
+iter=20
+i=1
+while [ $i -le $iter ]
+do
+	${EXE_FILE} ${EXE_ARGS}
+	((i++))
+	sleep 1
+done
+
+# collect time record info
+if [ -f time_record.txt ]
+then
+	echo "-----------------------------------------" >> total_time_record.txt
+	cat time_record.txt >> total_time_record.txt
+	echo "-----------------------------------------" >> total_time_record.txt
+	rm  -f time_record.txt
+else
+	echo "time_record.txt not found"
+fi
+
+
---
+++ utc-0.1/benchapps/rgbyuv/utc/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# mpi command
+#MPI_RUN='mpirun -report-bindings -bind-to none' #socket:overload-allowed'
+#MPI_PROCS=1
+#MPI_OPTION="-host node2,node3,node4,node5 -N ${MPI_PROCS}"
+#MPI_OPTION="-N ${MPI_PROCS}  -hostfile myhosts -host node2,node3,node4,node5"
+
+
+# set exe file and args
+EXE_FILE='./yuvconvert'
+EXE_ARGS="-i ../../image-input/imgl.ppm -m 2"
+
+# set iterations and run
+iter=30
+i=1
+while [ $i -le $iter ]
+do
+	${EXE_FILE} ${EXE_ARGS}
+	((i++))
+	sleep 2
+done
+
+# collect time record info
+if [ -f time_record.txt ]
+then
+	echo "-----------------------------------------" >> total_time_record.txt
+	cat time_record.txt >> total_time_record.txt
+	echo "-----------------------------------------" >> total_time_record.txt
+	rm  -f time_record.txt
+else
+	echo "time_record.txt not found"
+fi
+
+
